fab0aadf0532538d9369c38f3faac4ed7b4aa8a1
Jackie Liu <ljinke@gmail.com>
2013-08-22 15:46:18 +0800

    Added inital code

---
+++ XQueryDemo/Main.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace XQueryDemo
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.textContent = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textXPath = new System.Windows.Forms.TextBox();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.textNS = new System.Windows.Forms.TextBox();
+            this.textNSUri = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 102);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(753, 459);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonBrowse);
+            this.tabPage1.Controls.Add(this.textContent);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(745, 433);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Content";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBrowse.Location = new System.Drawing.Point(664, 404);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 1;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // textContent
+            // 
+            this.textContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textContent.Location = new System.Drawing.Point(6, 6);
+            this.textContent.Multiline = true;
+            this.textContent.Name = "textContent";
+            this.textContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textContent.Size = new System.Drawing.Size(733, 392);
+            this.textContent.TabIndex = 0;
+            this.textContent.Text = resources.GetString("textContent.Text");
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textResult);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(745, 433);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Result";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textResult
+            // 
+            this.textResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textResult.Location = new System.Drawing.Point(6, 6);
+            this.textResult.Multiline = true;
+            this.textResult.Name = "textResult";
+            this.textResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textResult.Size = new System.Drawing.Size(733, 472);
+            this.textResult.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.textBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(745, 433);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Help";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(739, 427);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "XPath";
+            // 
+            // textXPath
+            // 
+            this.textXPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textXPath.Location = new System.Drawing.Point(77, 9);
+            this.textXPath.Name = "textXPath";
+            this.textXPath.Size = new System.Drawing.Size(688, 20);
+            this.textXPath.TabIndex = 2;
+            this.textXPath.Text = "//cd";
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGo.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonGo.Location = new System.Drawing.Point(690, 35);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 23);
+            this.buttonGo.TabIndex = 3;
+            this.buttonGo.Text = "GO";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // textNS
+            // 
+            this.textNS.Location = new System.Drawing.Point(77, 38);
+            this.textNS.Name = "textNS";
+            this.textNS.Size = new System.Drawing.Size(452, 20);
+            this.textNS.TabIndex = 4;
+            // 
+            // textNSUri
+            // 
+            this.textNSUri.Location = new System.Drawing.Point(77, 64);
+            this.textNSUri.Name = "textNSUri";
+            this.textNSUri.Size = new System.Drawing.Size(452, 20);
+            this.textNSUri.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "NameSpace";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.textBox2);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(745, 433);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Exercise";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(0, 0);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(745, 433);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = resources.GetString("textBox2.Text");
+            // 
+            // Main
+            // 
+            this.AcceptButton = this.buttonGo;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 573);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textNSUri);
+            this.Controls.Add(this.textNS);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.textXPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Main";
+            this.Text = "XPath/XQuery Demo";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textXPath;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.TextBox textContent;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textNS;
+        private System.Windows.Forms.TextBox textNSUri;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ XQueryDemo/Main.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Xml;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XQueryDemo
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.textResult.Text = "";
+
+                XPathDocument doc = new XPathDocument(new System.IO.StringReader(this.textContent.Text));
+                XPathNavigator nvg = doc.CreateNavigator();
+                XPathExpression xpath = nvg.Compile(this.textXPath.Text);
+                if (!string.IsNullOrEmpty(this.textNS.Text) && !string.IsNullOrEmpty(this.textNSUri.Text))
+                {
+                    string[] names = this.textNS.Text.Split(';');
+                    string[] uris = this.textNSUri.Text.Split(';');
+                    if (names.Length == uris.Length)
+                    {
+                        XmlNamespaceManager nsMgr = new XmlNamespaceManager(nvg.NameTable);
+                        for (int i = 0; i < names.Length; i++)
+                        {
+                            nsMgr.AddNamespace(names[i], uris[i]);
+                        }
+                        xpath.SetContext(nsMgr);
+                    }
+                    else
+                        MessageBox.Show("Namespace mismatches.");
+                }
+                XPathNodeIterator iterator = nvg.Select(xpath);
+
+
+                while (iterator.MoveNext())
+                {
+                    
+                    this.textResult.Text += iterator.Current.Value;
+                    this.textResult.Text += Environment.NewLine;
+                }
+
+                this.tabControl1.SelectedIndex = 1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            string file = null;
+
+            FileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                file = dlg.FileName;
+
+                this.textContent.Text = System.IO.File.ReadAllText(file);
+
+                
+
+            }
+        }
+    }
+}
